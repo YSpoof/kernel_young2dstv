7bbb57aa14a09f03cbedb8ca14836a39e937890f
YSpoof <guuh@linuxmail.org>
2020-06-22 19:24:33 -0300

    Test comit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Mon Sep 15 15:08:27 KST 2014"
+#define LINUX_COMPILE_BY "yhenwoojung"
+#define LINUX_COMPILE_HOST "yhenwoojung"
+#define LINUX_COMPILER "gcc version 4.7.2 20120731 (prerelease) (Linaro GCC 4.7-2012.08) "
